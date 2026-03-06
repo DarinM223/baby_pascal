a9a809eb5ea51d288cab415d01cebd6db60a1828
d <darinm223@gmail.com>
2026-03-06 06:56:49 -0800

    Fill loop nesting using Tarjan's algorithm

--- lib/loopnesting.ml
+++ lib/loopnesting.ml
@@ -33,13 +33,14 @@ module Make
   let back_preds = Array.make Dom.size PositionSet.empty
   let non_back_preds = Array.make Dom.size PositionSet.empty
   let link = Array.init Dom.size (fun p -> p)
-  let _find n =
+  let header = Array.make Dom.size 0
+  let find n =
     let n = ref n in
     while link.(!n) <> !n do
       n := link.(!n)
     done;
     !n
-  let _union n m =
+  let union n m =
     let n = ref n in
     while link.(!n) <> !n do
       n := link.(!n)
@@ -62,4 +63,31 @@ module Make
         List.fold_left (fun acc p -> PositionSet.add p acc) !result headers
     done;
     !result
+
+  let _ =
+    for w = Dom.size - 1 downto 0 do
+      let p = ref PositionSet.empty in
+      PositionSet.iter
+        (fun v -> if v <> w then p := PositionSet.add (find v) !p)
+        back_preds.(w);
+      let q = ref !p in
+      while not (PositionSet.is_empty !q) do
+        let x = PositionSet.min_elt !q in
+        q := PositionSet.remove x !q;
+        PositionSet.iter
+          (fun y ->
+            let y = find y in
+            if not (Dom.dominates w y) then failwith "irreducible graph"
+            else if (not (PositionSet.mem y !p)) && y <> w then begin
+              p := PositionSet.add y !p;
+              q := PositionSet.add y !q
+            end)
+          non_back_preds.(x)
+      done;
+      PositionSet.iter
+        (fun x ->
+          header.(x) <- w;
+          union x w)
+        !p
+    done
 end
