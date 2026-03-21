0278a98aa93ffb695c23d6c7f884c9401c713898
d <darinm223@gmail.com>
2026-03-21 07:10:50 -0700

    Build out spill test instructions

--- test/test_spill.ml
+++ test/test_spill.ml
@@ -16,18 +16,90 @@ let test_loops () =
     in
     (* pass_through is variables not used in either loops
        but used at the beginning and end of the program *)
-    let _pass_through, temps =
+    let pass_through, temps =
       (Array.of_list (List.take 2 temps), List.drop 2 temps)
     in
     (* first_loop_temps are used in the body of the
        first loop and at the end of the program *)
     (* second_loop_temps are used at the beginning
        of the program and in the body of the second loop*)
-    let _first_loop_temps, _second_loop_temps =
+    let first_loop_temps, second_loop_temps =
       (Array.of_list (List.take 8 temps), Array.of_list (List.drop 8 temps))
     in
-    unfocus @@ init_temps @@ focus_entry empty
+    state.curr_block := 6;
+    let temp1 = state.fresh_vreg Int in
+    let temp2 = state.fresh_vreg Int in
+    let temp3 = state.fresh_vreg Int in
+    state.curr_block := 0;
+    unfocus @@ init_temps
+    @@ branch (1, "label1")
+    @@ label (1, "label1")
+    @@ cbranch
+         ~args:[ Reg first_loop_temps.(1); Reg first_loop_temps.(0) ]
+         LT ~ifso:(3, "label3") ~ifnot:(2, "label2")
+    @@ label (2, "label2")
+    @@ instruction
+         (instr "addq"
+            ~defs:[ Reg first_loop_temps.(1) ]
+            ~uses:[ Reg first_loop_temps.(0); Imm 1 ])
+    @@ instruction
+         (instr "subq"
+            ~defs:[ Reg first_loop_temps.(2) ]
+            ~uses:[ Reg first_loop_temps.(1); Reg first_loop_temps.(0) ])
+    @@ instruction
+         (instr "mulq"
+            ~defs:[ Reg first_loop_temps.(3) ]
+            ~uses:[ Reg first_loop_temps.(2); Reg first_loop_temps.(2) ])
+    @@ instruction
+         (instr "addq"
+            ~defs:[ Reg first_loop_temps.(5) ]
+            ~uses:[ Reg first_loop_temps.(3); Reg first_loop_temps.(4) ])
+    @@ instruction
+         (instr "subq"
+            ~defs:[ Reg first_loop_temps.(7) ]
+            ~uses:[ Reg first_loop_temps.(5); Reg first_loop_temps.(6) ])
+    @@ branch (1, "label1")
+    @@ label (4, "label4")
+    @@ cbranch
+         ~args:[ Reg second_loop_temps.(1); Reg second_loop_temps.(0) ]
+         LT ~ifso:(6, "label6") ~ifnot:(5, "label5")
+    @@ label (5, "label5")
+    @@ instruction
+         (instr "addq"
+            ~defs:[ Reg second_loop_temps.(1) ]
+            ~uses:[ Reg second_loop_temps.(0); Imm 1 ])
+    @@ instruction
+         (instr "subq"
+            ~defs:[ Reg second_loop_temps.(2) ]
+            ~uses:[ Reg second_loop_temps.(1); Reg second_loop_temps.(0) ])
+    @@ instruction
+         (instr "mulq"
+            ~defs:[ Reg second_loop_temps.(3) ]
+            ~uses:[ Reg second_loop_temps.(2); Reg second_loop_temps.(2) ])
+    @@ instruction
+         (instr "addq"
+            ~defs:[ Reg second_loop_temps.(5) ]
+            ~uses:[ Reg second_loop_temps.(3); Reg second_loop_temps.(4) ])
+    @@ instruction
+         (instr "subq"
+            ~defs:[ Reg second_loop_temps.(7) ]
+            ~uses:[ Reg second_loop_temps.(5); Reg second_loop_temps.(6) ])
+    @@ label (3, "label3")
+    @@ branch (4, "label4")
+    @@ branch (4, "label4")
+    @@ label (6, "label6")
+    @@ instruction
+         (instr "addq" ~defs:[ Reg temp1 ]
+            ~uses:[ Reg pass_through.(0); Reg pass_through.(1) ])
+    @@ instruction
+         (instr "addq" ~defs:[ Reg temp2 ]
+            ~uses:[ Reg first_loop_temps.(7); Reg temp1 ])
+    @@ instruction
+         (instr "addq" ~defs:[ Reg temp3 ]
+            ~uses:[ Reg second_loop_temps.(7); Reg temp2 ])
+    @@ return ~uses:[ Reg temp3 ] @@ focus_entry empty
   in
+  Format.printf "%a\n" X86.Printer.pp_graph cfg;
   let extra = X86.Cfg.precalculate_edges cfg in
   let module Extra = (val extra) in
   let module Dom = Dominator.Make (X86.Cfg) (Extra) in
