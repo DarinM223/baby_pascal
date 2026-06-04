d86f4eef3e74bd0556497eab3567e63b2fae76e7
d <darinm223@gmail.com>
2026-06-04 12:11:26 -0700

    Start compile function

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,7 +1,59 @@
-let compile _program =
-  (* let module Fresh = Normalize.Fresh () in *)
-  (* let = Normalize.normalize Fresh.fresh expr in *)
-  (* let program =
-    { program with Ast.main = Normalize.normalize Fresh.fresh program.Ast.main }
-  in *)
-  failwith ""
+let compile program =
+  let module F = Normalize.Fresh () in
+  let program =
+    let open Ast in
+    let normalize_decl = function
+      | Function (f, ps, t, body) ->
+        Function (f, ps, t, Normalize.(set_return f (normalize F.fresh body)))
+      | Procedure (f, ps, body) ->
+        Procedure (f, ps, Normalize.normalize F.fresh body)
+    in
+    {
+      program with
+      main = Normalize.normalize F.fresh program.main;
+      decls = List.map normalize_decl program.decls;
+    }
+  in
+  let rec round args cfg =
+    let cfg, changed = Deadcode.M.deadcode cfg in
+    let block_args = Constprop.block_args cfg in
+    let cfg, changed' = Constprop.constprop block_args args cfg in
+    if changed || changed' then round args cfg else cfg
+  in
+  let lower_cfg args cfg =
+    let extra = Normalize.Cfg.precalculate_edges cfg in
+    let module Extra = (val extra) in
+    let module Dom = Dominator.Make (Normalize.Cfg) (Extra) in
+    let a_orig = Construct.calc_a_orig cfg in
+    let live = Construct.calc_live cfg in
+    let cfg = Construct.insert_phis_pruned live (module Dom) a_orig cfg in
+    let cfg = Construct.rename_variables (module Dom) cfg in
+    let args = List.map (fun arg -> (arg, 0)) args in
+    let cfg = round args cfg in
+    let cfg =
+      Normalize.Cfg.Blocks.fold
+        (fun _ block acc -> Undag.Cfg.Blocks.insert (Undag.undag block) acc)
+        cfg Undag.Cfg.empty
+    in
+    let state = Select_x86.State.init () in
+    let srcs, cfg = Select_x86.codegen_function ~args state cfg in
+    let extra = X86.Cfg.precalculate_edges cfg in
+    let module Dom = Dominator.Make (X86.Cfg) ((val extra)) in
+    let module Loop = Loopnesting.Make (X86.Cfg) (Dom) in
+    let cfg =
+      Regalloc.(spill_helper ~args:(reg_ops srcs) (module Loop) state cfg)
+    in
+    let cfg = Regalloc.regalloc_helper (module Loop) state cfg (fun _ -> ()) in
+    (* todo: cleanup redundant moves, moves with two memory operands, lower parallel copies, etc *)
+    cfg
+  in
+  let lower_decl = function
+    | Ast.Function (f, args, ret, body) ->
+      Ast.Function (f, args, ret, lower_cfg (List.map fst args) body)
+    | Ast.Procedure (f, args, body) ->
+      Ast.Procedure (f, args, lower_cfg (List.map fst args) body)
+  in
+  let decls = List.map lower_decl program.decls in
+  let main = lower_cfg [] program.main in
+  (* todo: write out program to assembly file *)
+  { program with decls; main }
