ebc271ca6428a65ca96259b9d07ca2d8d6f2a3e5
d <darinm223@gmail.com>
2026-07-03 07:30:14 -0700

    Separate register shuffle setup for unit test

--- test/test_regalloc.ml
+++ test/test_regalloc.ml
@@ -5,21 +5,30 @@ open Baby_pascal
          registers live after enforce_constraints,
          assigned registers, etc *)
 
-let test_register_shuffle1 () =
-  (* let enforce_constraints_pcopy state uid instr_num instr = *)
+type reg_mapping = (X86.Target.physical_reg * bool) list
+
+let setup_register_shuffle ~(regs : X86.Target.reg array)
+    ~(extra_curr_live : reg_mapping) ~(uses : reg_mapping) ~(defs : reg_mapping)
+    ~(extra_clobbered_regs : reg_mapping) =
   let cfg = X86.Cfg.empty in
   let extra = X86.Cfg.precalculate_edges cfg in
   let module Dom = Dominator.Make (X86.Cfg) ((val extra)) in
   let select_state = Select_x86.State.init () in
   let block_execution_frequency _uid = 1. in
-  let num_vregs = 25 in
+  let num_vregs =
+    List.(
+      length extra_curr_live + length uses + length defs
+      + length extra_clobbered_regs)
+  in
   let uid, instr_num = (2, 5) in
   select_state.curr_block := uid;
   let vregs = Array.init num_vregs (fun _ -> select_state.fresh_vreg Int) in
-  let live_through = CCBV.create ~size:num_vregs false in
+  let live_through = Utils.IntHashtbl.create num_vregs in
   let dies _uid = function
     | X86.Target.Virtual vreg ->
-      Some (if CCBV.get live_through vreg.id then instr_num + 1 else instr_num)
+      Some
+        (if Utils.IntHashtbl.mem live_through vreg.id then instr_num + 1
+         else instr_num)
     | _ -> None
   in
   let liveness =
@@ -32,18 +41,11 @@ let test_register_shuffle1 () =
         max_register_pressure = (fun _ -> 0);
       }
   in
-  (* [ rax; rbx; rcx; rdx; rsi; rdi; rsp; rbp; r8; r9; r10; r11; r12; r13; r14; r15; ] *)
-  let regs =
-    X86.Regs.int_regs
-    |> List.map (fun phys -> X86.Target.Physical phys)
-    |> Array.of_list
-  in
   let open Regalloc in
   let state =
     init_state ~select_state ~regs ~block_execution_frequency ~liveness
       (module Dom)
   in
-  (* todo: set currently occupied registers in state *)
   let idx phys = find_reg_index regs (X86.Target.Physical phys) in
   let set_reg idx = function
     | X86.Target.Virtual vreg ->
@@ -52,70 +54,79 @@ let test_register_shuffle1 () =
       state.reg_current_pref.(idx) <- state.preferences.%(vreg.id).(idx)
     | _ -> failwith "Register not virtual"
   in
+  let next_vreg =
+    let c = ref (-1) in
+    fun () ->
+      incr c;
+      !c
+  in
+  let setup_occupied (reg, is_live_through) =
+    let vreg = vregs.(next_vreg ()) in
+    CCBV.set state.occupied (idx reg);
+    set_reg (idx reg) vreg;
+    if is_live_through then
+      Utils.IntHashtbl.replace live_through (X86.Target.index vreg) true;
+    X86.Target.Reg vreg
+  in
+  let setup_constrained (reg, is_live_through) =
+    let vreg = vregs.(next_vreg ()) in
+    ignore @@ X86.Target.constrained reg vreg;
+    if is_live_through then
+      Utils.IntHashtbl.replace live_through (X86.Target.index vreg) true;
+    X86.Target.Reg vreg
+  in
+  let srcs = CCVector.create () in
+  let dests = CCVector.create () in
+  List.iter (fun t -> ignore (setup_occupied t)) extra_curr_live;
+  List.iter (fun t -> CCVector.push srcs (setup_occupied t)) uses;
+  List.iter (fun t -> CCVector.push dests (setup_constrained t)) defs;
+  List.iter
+    (fun t -> CCVector.push dests (setup_constrained t))
+    extra_clobbered_regs;
+  let instr =
+    X86.Target.pcopy ~dests:(CCVector.to_list dests)
+      ~srcs:(CCVector.to_list srcs)
+  in
+  (vregs, enforce_constraints_pcopy state uid instr_num instr)
+
+let test_register_shuffle1 () =
+  (* [ rax; rbx; rcx; rdx; rsi; rdi; rsp; rbp; r8; r9; r10; r11; r12; r13; r14; r15; ] *)
+  let regs =
+    X86.Regs.int_regs
+    |> List.map (fun phys -> X86.Target.Physical phys)
+    |> Array.of_list
+  in
+  let idx phys = Regalloc.find_reg_index regs (X86.Target.Physical phys) in
   (* vregs 0, 1, 2 are live through but not in the uses or defs of the instruction *)
-  CCBV.set state.occupied (idx X86.Regs.rdi);
-  Format.printf "Vregs: %a\n" (CCArray.pp X86.Target.pp_reg) vregs;
-  Format.printf "Idx: %d\n" (idx X86.Regs.rdi);
-  set_reg (idx X86.Regs.rdi) vregs.(0);
-  CCBV.set live_through (X86.Target.index vregs.(0));
-
-  CCBV.set state.occupied (idx X86.Regs.rsi);
-  set_reg (idx X86.Regs.rsi) vregs.(1);
-  CCBV.set live_through (X86.Target.index vregs.(1));
-
-  CCBV.set state.occupied (idx X86.Regs.rdx);
-  set_reg (idx X86.Regs.rdx) vregs.(2);
-  CCBV.set live_through (X86.Target.index vregs.(2));
-
+  let extra_curr_live = X86.Regs.[ (rdi, true); (rsi, true); (rdx, true) ] in
   (* vregs 3, 4, 5, 6 are the uses of the instruction in callee save registers
      3, 4, and 6 die at the instruction, but 5 is live through *)
-  CCBV.set state.occupied (idx X86.Regs.r12);
-  set_reg (idx X86.Regs.r12) vregs.(3);
-  CCBV.set state.occupied (idx X86.Regs.r13);
-  set_reg (idx X86.Regs.r13) vregs.(4);
-  CCBV.set state.occupied (idx X86.Regs.rbx);
-  set_reg (idx X86.Regs.rbx) vregs.(5);
-  CCBV.set live_through (X86.Target.index vregs.(5));
-  CCBV.set state.occupied (idx X86.Regs.r9);
-  set_reg (idx X86.Regs.r9) vregs.(6);
-
-  check bool "Vreg 4 dies at instruction"
-    (dies state uid vregs.(4) instr_num)
-    true;
-  check bool "Vreg 5 doesn't die at instruction"
-    (dies state uid vregs.(5) instr_num)
-    false;
-
+  let uses =
+    X86.Regs.[ (r12, false); (r13, false); (rbx, true); (r9, false) ]
+  in
   (* vregs 7, 8, 9, 10 are constrained to rdi, rsi, rdx, rcx *)
-  ignore @@ X86.Target.constrained X86.Regs.rdi vregs.(7);
-  CCBV.set live_through (X86.Target.index vregs.(7));
-  ignore @@ X86.Target.constrained X86.Regs.rsi vregs.(8);
-  CCBV.set live_through (X86.Target.index vregs.(8));
-  ignore @@ X86.Target.constrained X86.Regs.rdx vregs.(9);
-  CCBV.set live_through (X86.Target.index vregs.(9));
-  ignore @@ X86.Target.constrained X86.Regs.rcx vregs.(10);
-  CCBV.set live_through (X86.Target.index vregs.(10));
-
+  let defs = X86.Regs.[ (rdi, true); (rsi, true); (rdx, true); (rcx, true) ] in
   (* vregs 11, 12, 13, 14, 15, 16, 17, 18, 19 are the constrained defs of the instruction
      the def for rax lives through the instruction but the rest die *)
-  ignore @@ X86.Target.constrained X86.Regs.rax vregs.(11);
-  CCBV.set live_through (X86.Target.index vregs.(11));
-  ignore @@ X86.Target.constrained X86.Regs.rcx vregs.(12);
-  ignore @@ X86.Target.constrained X86.Regs.r8 vregs.(13);
-  ignore @@ X86.Target.constrained X86.Regs.r9 vregs.(14);
-  ignore @@ X86.Target.constrained X86.Regs.r10 vregs.(15);
-  ignore @@ X86.Target.constrained X86.Regs.r11 vregs.(16);
-  ignore @@ X86.Target.constrained X86.Regs.rdx vregs.(17);
-  ignore @@ X86.Target.constrained X86.Regs.rsi vregs.(18);
-  ignore @@ X86.Target.constrained X86.Regs.rdi vregs.(19);
-  let instr =
-    X86.Target.pcopy
-      ~dests:
-        ([ 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19 ]
-        |> List.map (fun i -> X86.Target.Reg vregs.(i)))
-      ~srcs:([ 3; 4; 5; 6 ] |> List.map (fun i -> X86.Target.Reg vregs.(i)))
+  let extra_clobbered_regs =
+    X86.Regs.
+      [
+        (rax, true);
+        (rcx, false);
+        (r8, false);
+        (r9, false);
+        (r10, false);
+        (r11, false);
+        (rdx, false);
+        (rsi, false);
+        (rdi, false);
+      ]
   in
-  let result = enforce_constraints_pcopy state uid instr_num instr in
+  let vregs, result =
+    setup_register_shuffle ~regs ~extra_curr_live ~uses ~defs
+      ~extra_clobbered_regs
+  in
+  let num_vregs = Array.length vregs in
   let result_testable =
     option
       X86.Target.(
@@ -153,7 +164,7 @@ let test_register_shuffle1 () =
         (List.map (fun i -> X86.Target.Reg vregs.(i)) [ 1; 0; 2; 3; 4; 5; 6 ])
   in
   let expected_subst =
-    RegMap.of_list
+    X86.Target.RegMap.of_list
       [
         (vregs.(0), new_r12);
         (vregs.(1), new_r13);
